5dc7d3c9de92abb13b4d7b54890163a74e5914d4
bitbucket-cores-vn <bitbucket.cores.vn@gmail.com>
2020-08-17 14:05:46 +0700

    udoate code

--- resources/views/search/resultSearchForm.blade.php
+++ resources/views/search/resultSearchForm.blade.php
@@ -35,7 +35,7 @@ a.cls:not(.collapsed):after {
                         $dirs = explode(',', $image);
                         //dd($roomVip->image ,$dirs); 
                     ?>
-                        <div class="img align-self-stretch" style="background-image: url({{$dirs[0]}}); background-size: cover; background-position: center; background-repeat: no-repeat;"> </div>
+                        <div class="img align-self-stretch" style="background-image: url({{asset('/uploads/'.$dirs[0])}}); background-size: cover; background-position: center; background-repeat: no-repeat;"> </div>
                     </div></a>
                     <div class="text pb-4 text-center">
                         <div class="faded">
